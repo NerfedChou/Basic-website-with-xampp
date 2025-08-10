ad99775a98a2af1fdd755219d5c71dac915bc8e9
janrhian102904@gmail.com <janrhian012904@gmail.com>
2025-08-10 21:00:33 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -4,11 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form</title>
+    <link rel="stylesheet" href="login.css">
 </head>
+
 <body>
-    <form action="welcome.php" method="post">
-        Name: <input type="text" name="name"><br><br>
-        Email: <input type="email" name="email"><br><br>
-        <input type="submit">
+    <header>
+    Head
+    </header>
+    <div class="form"> <!-- Region -->
+        <span class="login-form">
+            Login in With
+        </span>
+        <span class="loginArea">
+            Login Area
+        </span>
+   <!--     <form action="welcome.php" method="post">
+                Name: <input type="text" name="name"><br><br>
+                Email: <input type="email" name="email"><br><br>
+            <input type="submit"> -->
+    </div>
 </body>
 </html>
